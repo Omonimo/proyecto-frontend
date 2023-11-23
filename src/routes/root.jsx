878cb75d230ebebeb4efc5f3bef3195fd73c6aaf
vboxuser <vboxuser@alfredovm.myguest.virtualbox.org>
2023-11-23 04:14:33 -0600

    global pending UI

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,17 @@
 import {
-    Outlet,
-    Link,
-    useLoaderData,
-    Form,
-  } from "react-router-dom";
-  import { getContacts, createContact } from "../contacts";
-  
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+
   export async function action() {
     const contact = await createContact();
-    return { contact };
+    return redirect(`/contacts/${contact.id}/edit`);
   }
 export async function loader() {
   const contacts = await getContacts();
@@ -17,6 +20,8 @@ export async function loader() {
 
 export default function Root() {
     const { contacts } = useLoaderData();
+    const navigation = useNavigation();
+
     return (
       <>
         <div id="sidebar">
@@ -51,7 +56,14 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink to={`contacts/${contact.id}`} 
+                   className={({ isActive, isPending }) =>
+                   isActive
+                     ? "active"
+                     : isPending
+                     ? "pending"
+                     : ""
+                 }>
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -60,7 +72,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -73,6 +85,9 @@ export default function Root() {
 
         </div>
         <div id="detail">
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
             <Outlet/>
         </div>
       </>
