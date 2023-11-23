fb5b238efc09ca56f657ef31e59ecf839f26ad1e
vboxuser <vboxuser@alfredovm.myguest.virtualbox.org>
2023-11-22 21:17:54 -0600

    avance proyecto

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page"
+import Contact from "./routes/contact";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
